3223fbd758498c45c1b87ef8e123175dd9621f51
Alex Li <7alex7li@gmail.com>
2021-07-25 16:26:22 -0400

    Add binary search, update lazy tree

---
+++ code/BinarySearch.cpp
@@ -0,0 +1,18 @@
+// This code is guaranteed to work in the min number of ops
+// for any MAX that fits in an ll.
+ll MAX = 1LL << 63;
+// Binary search integers in the range [0, MAX)
+// for the last element satisfying condition.
+for (int j = 1LL << (int) (log2(MAX)); j != 0; j >>= 1) {
+    if (condition(lo + j)) {
+        lo += j;
+    }
+}
+// Binary search integers in the range (1, MAX]
+// for the first element satisfying condition.
+ll hi = 1LL << (int) (log2(MAX) + 1);
+for (int j = 1LL << (int) (log2(MAX)); j != 0; j >>= 1) {
+    if (condition(0, hi - j)) {
+        hi -= j;
+    }
+}
--- code/SegmentTreeLazy.cpp
+++ code/SegmentTreeLazy.cpp
@@ -2,10 +2,13 @@
 using namespace std;
 typedef vector<int> vi;
 const int inf = 1e9;
+// A lazy segment tree supporting range add, range set, and range get max
 struct Node {
     Node *l = 0, *r = 0;
     int lo, hi, mset = inf, madd = 0, val = -inf;
     Node(int lo,int hi):lo(lo),hi(hi){} // Large interval of -inf
+    // Initialize based on the values in the vector v.
+    // main will call this with Node(v, 0, v.size())
     Node(vi& v, int lo, int hi) : lo(lo), hi(hi) {
         if (lo + 1 < hi) {
             int mid = lo + (hi - lo)/2;
@@ -14,23 +17,30 @@ struct Node {
         }
         else val = v[lo];
     }
+    // query [L, R)
     int query(int L, int R) {
         if (R <= lo || hi <= L) return -inf;
         if (L <= lo && hi <= R) return val;
         push();
         return max(l->query(L, R), r->query(L, R));
     }
+    // set all elements in [L, R) to x
     void set(int L, int R, int x) {
         if (R <= lo || hi <= L) return;
-        if (L <= lo && hi <= R) mset = val = x, madd = 0;
+        if (L <= lo && hi <= R) {
+            // Update the range [lo, hi) to x
+            mset = val = x, madd = 0;
+        }
         else {
             push(), l->set(L, R, x), r->set(L, R, x);
             val = max(l->val, r->val);
         }
     }
+    // add x to all elements in [L, R)
     void add(int L, int R, int x) {
         if (R <= lo || hi <= L) return;
         if (L <= lo && hi <= R) {
+            // Add x to all elements in the range [lo, hi)
             if (mset != inf) mset += x;
             else madd += x;
             val += x;
@@ -40,6 +50,7 @@ struct Node {
             val = max(l->val, r->val);
         }
     }
+    // Push the lazily stored values.
     void push() {
         if (!l) {
             int mid = lo + (hi - lo)/2;
