2d307c4208557591ba4f99318bf8e1d89499ff77
Alex Li <7alex7li@gmail.com>
2021-07-31 12:27:41 -0400

    Change font siz'

--- code/BinarySearch.cpp
+++ code/BinarySearch.cpp
@@ -1,17 +1,18 @@
 // This code is guaranteed to work in the min number of ops
 // for any MAX that fits in an ll.
-ll MAX = 1LL << 63;
-// Binary search integers in the range [0, MAX)
+ll MAX = 1LL << 62;
+// Binary search integers in the range [0, MAX] (or higher)
 // for the last element satisfying condition.
-for (int j = 1LL << (int) (log2(MAX)); j != 0; j >>= 1) {
+ll lo = 0
+for (ll j = 1LL << (ll) (log2(MAX)); j != 0; j >>= 1) {
     if (condition(lo + j)) {
         lo += j;
     }
 }
-// Binary search integers in the range (1, MAX]
+// Binary search integers in the range (1, MAX] (or higher)
 // for the first element satisfying condition.
-ll hi = 1LL << (int) (log2(MAX) + 1);
-for (int j = 1LL << (int) (log2(MAX)); j != 0; j >>= 1) {
+ll hi = 1LL << (ll) (log2(MAX) + 1);
+for (ll j = 1LL << (ll) (log2(MAX)); j != 0; j >>= 1) {
     if (condition(hi - j)) {
         hi -= j;
     }
--- code/UnionFind.cc
+++ code/UnionFind.cc
@@ -10,6 +10,10 @@ struct UF {
     bool sameSet(int a, int b) { return find(a) == find(b); }
     int size(int x) { return -e[find(x)]; }
     int find(int x) { return e[x] < 0 ? x : e[x] = find(e[x]); }
+    int find2(int x) { // Dennis's faster union find
+        while (e[x] >= 0) {e[x] = e[e[x]]; x = e[x]}
+        return x;
+    }
     bool join(int a, int b) {
         a = find(a), b = find(b);
         if (a == b) return false;
